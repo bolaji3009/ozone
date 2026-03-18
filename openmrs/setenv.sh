e54806b4820c64a0e98712f47d3f52ae935cd848
Your Name <you@example.com>
2026-03-18 22:52:22 +0000

    fix: Robust OpenMRS startup with custom setenv.sh and long curl loop

---
+++ openmrs/setenv.sh
@@ -0,0 +1,2 @@
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms512m -Xmx3g -XX:PermSize=256m -XX:MaxPermSize=512m"
+export CATALINA_OPTS="-DOPENMRS_INSTALLATION_SCRIPT=/usr/local/tomcat/openmrs-server.properties -DOPENMRS_APPLICATION_DATA_DIRECTORY=/usr/local/tomcat/.OpenMRS"
